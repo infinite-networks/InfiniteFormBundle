2a5a2635ce7486d389a8647d7bcb896f84f23252
Christophe Coevoet <stof@notk.org>
2015-08-27 18:34:53 +0200

    Remove usage of deprecated Twig features

--- Tests/FormExtension/FormExtensionTest.php
+++ Tests/FormExtension/FormExtensionTest.php
@@ -19,27 +19,21 @@ class FormExtensionTest extends \PHPUnit_Framework_TestCase
     /** @var FormFactory */
     private $formFactory;
 
-    /** @var \Twig_Environment */
-    private $twig;
-
     public function setUp()
     {
         $this->formFactory = Forms::createFormFactory();
-
-        $this->twig = new \Twig_Environment;
-        $this->twig->addExtension(new FormExtension);
-        $this->twig->setLoader(new \Twig_Loader_String);
     }
 
     public function testInvalidTest()
     {
-        $template = $this->twig->loadTemplate('{{ form is invalid ? 1 : 0 }}');
+        $twig = new \Twig_Environment(new \Twig_Loader_Array(array('template' => '{{ form is invalid ? 1 : 0 }}')));
+        $twig->addExtension(new FormExtension);
 
         $formWithError = $this->formFactory->create('text');
         $formWithError->addError(new FormError('test error'));
-        $this->assertEquals('1', $template->render(array('form' => $formWithError->createView())));
+        $this->assertEquals('1', $twig->render('template', array('form' => $formWithError->createView())));
 
         $formWithoutError = $this->formFactory->create('text');
-        $this->assertEquals('0', $template->render(array('form' => $formWithoutError->createView())));
+        $this->assertEquals('0', $twig->render('template', array('form' => $formWithoutError->createView())));
     }
 }
